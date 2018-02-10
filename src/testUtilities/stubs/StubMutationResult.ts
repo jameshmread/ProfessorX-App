68a1a76616ce8efaa38d2470da7cde7c0b300edb
jameshmread <jamesread117@yahoo.co.uk>
2018-02-10 21:38:01 +0000

    added more mutation results

--- src/testUtilities/stubs/StubMutationResult.ts
+++ src/testUtilities/stubs/StubMutationResult.ts
@@ -1,6 +1,7 @@
 import { IMutationResult } from "../../../interfaces/IMutationResult";
 
 export class StubMutationResult {
+
       public static mutationResultSuccessKilled: IMutationResult = {
             lineNumber: 3,
             mutantKilled: true,
@@ -14,6 +15,19 @@ export class StubMutationResult {
             testFilePath: "Stub/Test/FilePath/"
       };
 
+      public static mutationResultSuccessKilled1: IMutationResult = {
+            lineNumber: 35,
+            mutantKilled: true,
+            origionalCode: "return true;",
+            mutatedCode: "return false;",
+            mutationAttemptFailure: "N/A",
+            numFailedTests: 1,
+            numPassedTests: 0,
+            srcFileName: "StubFileName.ts",
+            srcFilePath: "Stub/FilePath/",
+            testFilePath: "Stub/Test/FilePath/"
+      };
+
       public static mutationResultSuccessSurvived: IMutationResult = {
             lineNumber: 3,
             mutantKilled: false,
@@ -22,7 +36,33 @@ export class StubMutationResult {
             mutationAttemptFailure: "N/A",
             numFailedTests: 0,
             numPassedTests: 4,
+            srcFileName: "StubFileName.ts",
+            srcFilePath: "Stub/FilePath/",
+            testFilePath: "Stub/Test/FilePath/"
+      };
+
+      public static mutationResultSuccessSurvived1: IMutationResult = {
+            lineNumber: 3,
+            mutantKilled: false,
+            origionalCode: "a = x + y",
+            mutatedCode: "a = x * y;",
+            mutationAttemptFailure: "N/A",
+            numFailedTests: 0,
+            numPassedTests: 4,
             srcFileName: "StubFileNamesuccess.ts",
+            srcFilePath: "Stub/FilePath/",
+            testFilePath: "Stub/Test/FilePath/"
+      };
+
+      public static mutationResultSuccessSurvived2: IMutationResult = {
+            lineNumber: 33,
+            mutantKilled: false,
+            origionalCode: "a = x + y",
+            mutatedCode: "a = x / y;",
+            mutationAttemptFailure: "N/A",
+            numFailedTests: 0,
+            numPassedTests: 4,
+            srcFileName: "SuccessSurvive.ts",
             srcFilePath: "Stub/FilePath/",
             testFilePath: "Stub/Test/FilePath/"
       };
@@ -49,4 +89,12 @@ export class StubMutationResult {
             origionalCode: "return a + b;",
             mutatedCode: "return a + b;"
       };
+      public static allResults = [
+            StubMutationResult.mutationResultSuccessKilled,
+            StubMutationResult.mutationResultSuccessKilled1,
+            StubMutationResult.mutationResultSuccessSurvived,
+            StubMutationResult.mutationResultSuccessSurvived1,
+            StubMutationResult.mutationResultSuccessSurvived2,
+            StubMutationResult.mutationResultFail
+      ];
 }
