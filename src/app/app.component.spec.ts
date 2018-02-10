7f688dd4f25c8e885e4d9bde3397f353330c080d
jameshmread <jamesread117@yahoo.co.uk>
2018-02-10 15:10:34 +0000

    injected mutation results service into app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { TestBed, async } from "@angular/core/testing";
 import { AppComponent } from "./app.component";
 import * as data from "./outputStoreData.json";
 import * as newData from "./newData.json";
+import { MutationResultsService } from "../services/mutation-results.service";
 
 describe("AppComponent", () => {
   let component;
   beforeEach(() => {
-    component = new AppComponent();
+    component = new AppComponent(new MutationResultsService());
   });
 
   it("", () => {
