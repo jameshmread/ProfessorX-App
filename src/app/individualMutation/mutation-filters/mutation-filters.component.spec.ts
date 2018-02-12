5fbf9eb16ea37c26b6ea16b8267d690d83a449b9
jameshmread <jamesread117@yahoo.co.uk>
2018-02-12 19:53:42 +0000

    added mock to test

--- src/app/individualMutation/mutation-filters/mutation-filters.component.spec.ts
+++ src/app/individualMutation/mutation-filters/mutation-filters.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { MutationFiltersComponent } from "./mutation-filters.component";
+import { mock } from "ts-mockito/lib/ts-mockito";
+import { MutationResultsService } from "../../../services/mutation-results.service";
 
 describe("MutationFiltersComponent", () => {
 
       let mfc: MutationFiltersComponent;
       beforeEach( () => {
-            mfc = new MutationFiltersComponent();
+            mfc = new MutationFiltersComponent(mock(MutationResultsService));
       });
 
       it("should be created", () => {
