5d3553def97fb1bafa73a0e4f490788e66bc75db
jameshmread <jamesread117@yahoo.co.uk>
2018-02-12 17:01:47 +0000

    added enum for individual mutation properties

---
+++ enums/ResultFields.ts
@@ -0,0 +1,12 @@
+export enum ResultFields {
+    srcFilePath = "srcFilePath",
+    srcFileName = "srcFileName",
+    testFilePath= "testFilePath",
+    lineNumber= "lineNumber",
+    origionalCode= "origionalCode",
+    mutatedCode= "mutatedCode",
+    numPassedTests= "numPassedTests",
+    numFailedTests= "numFailedTests",
+    mutantKilled= "mutantKilled",
+    mutationAttemptFailure= "mutationAttemptFailure"
+}
