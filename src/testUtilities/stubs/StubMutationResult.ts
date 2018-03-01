f64bde7073208c6bca4c87a0485319f701cb1b9d
jameshmread <jamesread117@yahoo.co.uk>
2018-03-01 20:36:24 +0000

    now conforms to new mutation result

--- src/testUtilities/stubs/StubMutationResult.ts
+++ src/testUtilities/stubs/StubMutationResult.ts
@@ -2,28 +2,32 @@ import { IMutationResult } from "../../../interfaces/IMutationResult";
 
 export class StubMutationResult {
       public static mutationResultSuccessKilled: IMutationResult = {
-            lineNumber: 3, mutantKilled: true, origionalCode: ["a = x + y"], mutatedCode: ["a = x - y;"],
+            mutantKilled: true, origionalCode: [{lineText: "a = x + y", lineNumber: 1}],
+            mutatedCode: [{lineText: "a = x - y", lineNumber: 1}],
             mutationAttemptFailure: "N/A", numFailedTests: 3, numPassedTests: 1, srcFileName: "StubFileName.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessKilled1: IMutationResult = {
-            lineNumber: 35, mutantKilled: true, origionalCode: ["return true;"], mutatedCode: ["return false;"],
+            mutantKilled: true, origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
+            mutatedCode: [{lineText: "return false", lineNumber: 1}],
             mutationAttemptFailure: "N/A", numFailedTests: 1, numPassedTests: 0, srcFileName: "StubFileName.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessSurvived: IMutationResult = {
-            lineNumber: 3, mutantKilled: false, origionalCode: ["a = x + y"], mutatedCode: ["a = x - y;"],
+            mutantKilled: false, origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
+            mutatedCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutationAttemptFailure: "N/A", numFailedTests: 0, numPassedTests: 4, srcFileName: "StubFileName.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessSurvived1: IMutationResult = {
-            lineNumber: 3,
-            mutantKilled: false, origionalCode: ["a = x + y"], mutatedCode: ["a = x * y;"],
+            mutantKilled: false, origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
+            mutatedCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutationAttemptFailure: "N/A", numFailedTests: 0, numPassedTests: 4, srcFileName: "StubFileNamesuccess.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessSurvived2: IMutationResult = {
-            lineNumber: 33, mutantKilled: false, origionalCode: ["a = x + y"], mutatedCode: ["a = x / y;"],
+            mutantKilled: false, origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
+            mutatedCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutationAttemptFailure: "N/A", numFailedTests: 0, numPassedTests: 4, srcFileName: "SuccessSurvive.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
@@ -33,7 +37,8 @@ export class StubMutationResult {
             "N/A", mutationAttemptFailure: { attemptedMutation: "37 => -", nodeToBeMutated: {
                         syntaxType: 37, positions: {pos: 91, end: 93},
                         parentFileName: "HelloWorld.ts"}
-            }, lineNumber: 2, origionalCode: ["return a + b;"], mutatedCode: ["return a + b;"]
+            }, origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
+            mutatedCode: [{lineText: "return a + b;", lineNumber: 1}]
       };
       public static allResults = [
             StubMutationResult.mutationResultSuccessKilled, StubMutationResult.mutationResultSuccessKilled1,
