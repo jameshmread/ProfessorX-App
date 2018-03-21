e7cd2ae903d95c708e24feb9178d233ddca4dd19
jameshmread <jamesread117@yahoo.co.uk>
2018-03-21 20:24:24 +0000

    removed mutation score calculator and replaced with new service call

--- src/app/dashboard/mutation-stats-summary/mutation-stats-summary.component.spec.ts
+++ src/app/dashboard/mutation-stats-summary/mutation-stats-summary.component.spec.ts
@@ -17,34 +17,4 @@ describe("MutationStatsSummaryComponent", () => {
   it("should create", () => {
     expect(mssc).toBeTruthy();
   });
-
-  it("should = 100 given 1 killed and 0 surviving", () => {
-    mssc.killedMutants = 1;
-    mssc.survivingMutants = 0;
-    expect(mssc.setMutationScore()).toEqual(100);
-  });
-
-  it("should = 0 given 0 killed and 1 surviving", () => {
-    mssc.killedMutants = 0;
-    mssc.survivingMutants = 1;
-    expect(mssc.setMutationScore()).toEqual(0);
-  });
-
-  it("should = 50 given 1 killed and 1 surviving", () => {
-    mssc.killedMutants = 1;
-    mssc.survivingMutants = 1;
-    expect(mssc.setMutationScore()).toEqual(50);
-  });
-
-  it("should = 50 given 1 killed and 1 surviving", () => {
-    mssc.killedMutants = 1;
-    mssc.survivingMutants = 1;
-    expect(mssc.setMutationScore()).toEqual(50);
-  });
-
-  it("should return 66.6 given 2 killed and 1 surviving", () => {
-    mssc.killedMutants = 2;
-    mssc.survivingMutants = 1;
-    expect(mssc.setMutationScore()).toEqual(66.67);
-  });
 });
