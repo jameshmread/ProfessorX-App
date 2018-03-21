28077b22f1b9f88c184b8b1d82aeda29052cfe1c
jameshmread <jamesread117@yahoo.co.uk>
2018-03-21 20:01:33 +0000

    mresults service can now be queried for summary files

--- src/services/mutation-results.service.ts
+++ src/services/mutation-results.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from "@angular/core";
 import { IMutationResult } from "../../interfaces/IMutationResult";
 import { IRunnerConfig } from "../../interfaces/IRunnerConfig";
-
+import { ISummary } from "../../interfaces/ISummary";
+import { IFileList } from "../../interfaces/IFileList";
 @Injectable()
 export class MutationResultsService {
 
   private mutationResults: Array<IMutationResult>;
   private testRunnerConfig: IRunnerConfig;
+  private summaryFiles: Array<IFileList>;
+  private overallScores: ISummary;
+
   constructor () {
 
   }
@@ -17,6 +21,11 @@ export class MutationResultsService {
 
   public setTestRunnerConfig (runnerConfig: IRunnerConfig): void {
     this.testRunnerConfig = runnerConfig;
+  }
+
+  public setSummaryInfo (files: Array<IFileList>, overallScores: ISummary) {
+    this.summaryFiles = files;
+    this.overallScores = overallScores;
   }
 
   public getAllMutationResults (): Array<IMutationResult> {
@@ -43,5 +52,13 @@ export class MutationResultsService {
   public getSurvivorsByFilter (attributeToFilterBy: string, value: any) {
     return this.getAllSurvivingMutants()
     .filter((result) => result[attributeToFilterBy] === value);
+  }
+
+  public getSummaryFiles (): Array<IFileList> {
+    return this.summaryFiles;
+  }
+
+  public getOverallSummary (): ISummary {
+    return this.overallScores;
   }
 }
