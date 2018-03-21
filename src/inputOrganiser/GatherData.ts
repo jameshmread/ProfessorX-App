a316ad2c6df0aa5b1fdf8891ffdc9dda0aede4a9
jameshmread <jamesread117@yahoo.co.uk>
2018-03-21 19:59:35 +0000

    input organiser now extracts new summary information

--- src/inputOrganiser/GatherData.ts
+++ src/inputOrganiser/GatherData.ts
@@ -1,9 +1,14 @@
-import { InputOrganiser } from "./InputOrganiser";
 import { IRunnerConfig } from "../../interfaces/IRunnerConfig";
 import { IMutationResult } from "../../interfaces/IMutationResult";
-import * as data from "../app/devData.json";
+import { ISummary } from "../../interfaces/ISummary";
+import { IFileList } from "../../interfaces/IFileList";
+
+import { InputOrganiser } from "./InputOrganiser";
+import * as data from "../app/newData.json";
+// import * as data from "../app/devData.json";
 
 export class GatherData extends InputOrganiser {
+
       constructor (){
             super(data);
       }
@@ -13,24 +18,26 @@ export class GatherData extends InputOrganiser {
                   runnerConfig: this.inputData[0]["config"]
             };
       }
+
       public getResultsArray (): Array<IMutationResult> {
             const resultsArray = new Array<IMutationResult>();
             this.inputData[1].forEach((result) => {
                   result = this.fillNonApplicableFields(result);
                   resultsArray.push({
-                  srcFilePath: result["SRC_FILE_PATH"],
-                  srcFileName: result["SRC_FILE"],
-                  testFilePath: result["testFilePath"],
-                  origionalCode: result["origionalCode"],
-                  mutatedCode: result["mutatedCode"],
-                  numPassedTests: result["numberPassedTests"],
-                  numFailedTests: result["numberFailedTests"],
-                  mutantKilled: result["mutantKilled"],
-                  mutationAttemptFailure: result["mutationAttemptFailure"]
+                        srcFilePath: result["SRC_FILE_PATH"],
+                        srcFileName: result["SRC_FILE"],
+                        testFilePath: result["testFilePath"],
+                        origionalCode: result["origionalCode"],
+                        mutatedCode: result["mutatedCode"],
+                        numPassedTests: result["numberPassedTests"],
+                        numFailedTests: result["numberFailedTests"],
+                        mutantKilled: result["mutantKilled"],
+                        mutationAttemptFailure: result["mutationAttemptFailure"]
                   });
             });
             return resultsArray;
       }
+
       public fillNonApplicableFields (result: IMutationResult) {
             for (const element in result) {
                   if (result[element] === void 0) {
@@ -46,5 +53,13 @@ export class GatherData extends InputOrganiser {
               duration.push(this.inputData[0]["duration"][timeDivision]);
             });
             return duration;
+      }
+
+      public getSummaryFileList (): Array<IFileList> {
+            return this.inputData[0]["fileList"];
+      }
+
+      public getOverallScores (): ISummary {
+            return this.inputData[0]["overallScores"];
       }
 }
