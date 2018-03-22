cdd1c98c2b35deaa0543ac16bad38dca18377607
jameshmread <jamesread117@yahoo.co.uk>
2018-03-22 15:14:51 +0000

    mutated file summary now shows number of killed + total mutants per file

--- src/app/dashboard/mutated-files-summary/mutated-files-summary.component.ts
+++ src/app/dashboard/mutated-files-summary/mutated-files-summary.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { MutationResultsService } from "../../../services/mutation-results.service";
 import { Filters } from "../../../preProcessors/Filters";
-import { ResultFields } from "../../../../enums/ResultFields";
 
 @Component({
   selector: "app-mutated-files-summary",
@@ -15,10 +14,7 @@ export class MutatedFilesSummaryComponent implements OnInit {
   constructor (private mResults: MutationResultsService) { }
 
   public ngOnInit () {
-    this.sourceFiles =
-      Filters.removeArrayDuplicates(
-        Filters.getIndividualProperty(this.mResults.getAllMutationResults(), ResultFields.srcFileName)
-      );
+    this.sourceFiles = Filters.removeArrayDuplicates(this.mResults.getSummaryFiles());
   }
 
 }
