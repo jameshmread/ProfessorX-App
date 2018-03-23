4aade144201142b8b71b1fc826c86e84b6fbdcdc
jameshmread <jamesread117@yahoo.co.uk>
2018-03-23 19:28:17 +0000

    using devData

--- src/inputOrganiser/GatherData.ts
+++ src/inputOrganiser/GatherData.ts
@@ -4,7 +4,7 @@ import { ISummary } from "../../interfaces/ISummary";
 import { IFileList } from "../../interfaces/IFileList";
 
 import { InputOrganiser } from "./InputOrganiser";
-import * as data from "../app/newData.json";
+import * as data from "../app/devData.json";
 
 export class GatherData extends InputOrganiser {
 
