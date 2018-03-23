b0475cb923331bafb56689f3ace229d129f4b27f
jameshmread <jamesread117@yahoo.co.uk>
2018-03-23 17:38:46 +0000

    imutation result interface updated to remove num tests and mutant killed

--- interfaces/IMutationResult.ts
+++ interfaces/IMutationResult.ts
@@ -4,8 +4,5 @@ export interface IMutationResult {
       testFilePath: string;
       origionalCode: Array<{lineNumber: number, lineText: string}>;
       mutatedCode: Array<{lineNumber: number, lineText: string}>;
-      numPassedTests: number | string;
-      numFailedTests: number | string;
-      mutantKilled: boolean | string;
       mutationAttemptFailure: Object;
 }
