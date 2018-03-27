7f640cab8e4753704392bf70c50d31a465a688ab
jameshmread <jamesread117@yahoo.co.uk>
2018-03-27 21:24:38 +0100

    fixed tests with injectors for navbar summary service

--- src/app/individualMutation/code-change-display/code-change-display.component.spec.ts
+++ src/app/individualMutation/code-change-display/code-change-display.component.spec.ts
@@ -2,11 +2,13 @@ import { CodeChangeDisplayComponent } from "./code-change-display.component";
 import { mock } from "ts-mockito/lib/ts-mockito";
 import { MutationResultsService } from "../../../services/mutation-results.service";
 import { IndividualMutationService } from "../../../services/individual-mutation.service";
+import { NavbarSummaryService } from "../../../services/navbar-summary.service";
 
 describe("CodeChangeDisplayComponent", () => {
   let ccdc: CodeChangeDisplayComponent;
 
   beforeEach(() => {
-    ccdc = new CodeChangeDisplayComponent(mock(MutationResultsService), mock(IndividualMutationService));
+    ccdc = new CodeChangeDisplayComponent(
+      mock(MutationResultsService), mock(IndividualMutationService), mock(NavbarSummaryService));
   });
 });
--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { NavbarComponent } from "./navbar.component";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NavbarSummaryService } from "../../services/navbar-summary.service";
 
 describe("NavbarComponent", () => {
   let component: NavbarComponent;
@@ -10,7 +11,8 @@ describe("NavbarComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ NavbarComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [NavbarSummaryService]
     })
     .compileComponents();
   }));
