7bb69b62c51bf96a5714acc4c9457d187c7ac37d
jameshmread <jamesread117@yahoo.co.uk>
2018-03-30 17:23:32 +0100

    updated stub mutation results for updated imutation result

--- src/testUtilities/stubs/StubMutationResult.ts
+++ src/testUtilities/stubs/StubMutationResult.ts
@@ -2,36 +2,48 @@ import { IMutationResult } from "../../../interfaces/IMutationResult";
 
 export class StubMutationResult {
       public static mutationResultSuccessKilled: IMutationResult = {
+            mutationType: "",
+            targetNode: "",
             origionalCode: null,
             mutatedCode: null,
             mutationAttemptFailure: undefined, srcFileName: "StubFileName.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessKilled1: IMutationResult = {
+            mutationType: "",
+            targetNode: "",
             origionalCode: null,
             mutatedCode: null,
             mutationAttemptFailure: undefined, srcFileName: "StubFileName.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessSurvived: IMutationResult = {
+            mutationType: "",
+            targetNode: "",
             origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutatedCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutationAttemptFailure: undefined, srcFileName: "StubFileName.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessSurvived1: IMutationResult = {
+            mutationType: "",
+            targetNode: "",
             origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutatedCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutationAttemptFailure: undefined, srcFileName: "StubFileNamesuccess.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultSuccessSurvived2: IMutationResult = {
+            mutationType: "",
+            targetNode: "",
             origionalCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutatedCode: [{lineText: "return a + b;", lineNumber: 1}],
             mutationAttemptFailure: undefined, srcFileName: "SuccessSurvive.ts",
             srcFilePath: "Stub/FilePath/", testFilePath: "Stub/Test/FilePath/"
       };
       public static mutationResultFail: IMutationResult = {
+            mutationType: "",
+            targetNode: "",
             srcFilePath: "./testProject/src/", srcFileName: "HelloWorld.ts", testFilePath: null,
             mutationAttemptFailure: { attemptedMutation: "37 => -", nodeToBeMutated: {
                         syntaxType: 37, positions: {pos: 91, end: 93},
